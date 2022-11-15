31b230141698164a178e235a5157563931eaeed3
Alabi12 <wolo22001@gmail.com>
2022-11-15 23:17:12 +0000

    update the linter files to correct errors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-import "./style.css";
+import './style.css';
 
-const todoContainer = document.querySelector(".todo-list");
+const todoContainer = document.querySelector('.todo-list');
 
 const toDoItems = [
   {
-    description: "My first to do",
+    description: 'My first to do',
     completed: false,
     index: 0,
   },
   {
-    description: "My second to do",
+    description: 'My second to do',
     completed: false,
     index: 1,
   },
   {
-    description: "My third to do",
+    description: 'My third to do',
     completed: false,
     index: 2,
   },
 ];
 
-const itemMarkupGen = (data) => {
-  return `<li class="todo" data-index="${data.index}" data-completed="${data.completed}"> 
+const itemMarkupGen = (data) => `<li class="todo" data-index="${data.index}" data-completed="${data.completed}"> 
   <input type="checkbox" id="${data.index}" name="${data.index}" value="Bike">
   <label for="${data.index}"> ${data.description} <i
           class="fa-list-icon fa-solid fa-ellipsis-vertical"></i></label>
   </li>`;
-};
 
 const populateList = (arr) => {
-  let listString = "";
+  let listString = '';
 
   const sortedArr = arr.sort((a, b) => a.index - b.index);
 
